47edf6aef58113d349f22a86a1c72f8b4457466c
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-20 09:49:07 -0300

    Restart on fall

---
+++ Assets/RestartOnFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartOnFall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -25)
+        {
+            Debug.Log("Me caí");
+            SceneManager.LoadScene("FPSParkour");
+        }
+    }
+}
