cf429fa55f2429d7fbfe583d75e948545f965ee2
Federico Zonis <49090960@est.ort.edu.ar>
2025-07-08 00:00:40 -0300

    Con la e destruis cajas

---
+++ Assets/InteractionArea.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject interactionMessage;
+    public MercanciaScript mercancia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionMessage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (mercancia)
+            {
+                Destroy(mercancia.gameObject);
+                StopInteraction();
+            } 
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        mercancia = other.GetComponent<MercanciaScript>();
+
+        if (mercancia)
+        {
+               interactionMessage.SetActive(true);
+        }
+    }
+
+   private void OnTriggerExit(Collider other)
+    {
+        StopInteraction();
+    }
+    void StopInteraction()
+    {
+        interactionMessage.SetActive(false);
+        mercancia = null;
+    }
+}
